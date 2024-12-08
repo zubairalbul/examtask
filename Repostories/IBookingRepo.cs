8e987308a424f7cde2a55689a6482e1836870936
Zubair Al Bulushi <zubair.albu@gmial.com>
2024-12-08 10:26:15 +0400

    Booking interface has been created

---
+++ Repostories/IBookingRepo.cs
@@ -0,0 +1,11 @@
+﻿using examtask.Models;
+
+namespace examtask.Repostories
+{
+    public interface IBookingRepo
+    {
+        (DateTime, string, string) Add(Bookings appointment);
+        IEnumerable<Bookings> GetAll();
+        Bookings GetById(int id);
+    }
+}
